0b362840e657c32b4d29ea1b2863d1f1544ddb81
fyqing <fyq@berkeley.edu>
2025-07-25 13:10:54 -0700

    initial version

--- BerkeleyLeanCamp/Basic.lean
+++ BerkeleyLeanCamp/Basic.lean
@@ -1,1 +1,12 @@
-def hello := "world"
+-- Import necessary libraries
+import Mathlib
+
+-- Use PUnit which has built-in topology
+def ZeroDimModel : Type* := PUnit
+
+-- PUnit automatically gets the discrete topology
+instance : DiscreteTopology ZeroDimModel := ⟨fun s => trivial⟩
+
+-- Main variables
+variable {M : Type*} [TopologicalSpace M] [T2Space M] [SecondCountableTopology M]
+variable [ChartedSpace ZeroDimModel M]
